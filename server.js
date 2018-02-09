9095d3bb5fcb5db22f8c8bab1950cc16fcfcc249
jonlord <jonlord748@gmail.com>
2018-02-09 12:58:22 +0100

    Complete lecture 18

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const ejs = require('ejs')
 const container = require('./container')
-const router = require('express-promise-router')();
+const router = require('express-promise-router')()
+const cookieParser = require('cookie-parser')
+const validator = require('express-validator')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const mongoose = require('mongoose')
+const flash = require('connect-flash')
+const passport = require('passport')
+
 
 container.resolve(function(users) {
+  mongoose.connect('mongodb://me:me@127.0.0.1:27017/footballer');
+
   const app = setupExpress();
 
   function setupExpress() {
@@ -24,8 +34,21 @@ container.resolve(function(users) {
     app.set('view engine', 'ejs');
     app.use(express.json());
     app.use(express.urlencoded({extended: true  }));
+
+    app.use(validator());
+    app.use(session({
+      secret: 'thomasinsegreto',
+      resave: true,
+      saveUninitialized: true,
+      store: new MongoStore(
+        {
+          mongooseConnection: mongoose.connection
+        }
+      )
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(flash());
   }
 
 })
-
-// app.get('/', (req, res) => res.send('Hello World!'))
