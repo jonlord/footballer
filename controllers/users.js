73a66b3830394f5b707edfac0fd01ecd18342273
jonlord <jonlord748@gmail.com>
2018-02-09 13:05:59 +0100

    Complete lecture 19

--- controllers/users.js
+++ controllers/users.js
@@ -4,9 +4,13 @@ module.exports = function(_) {
   return {
     setRouting: function(router) {
       router.get('/', this.indexPage);
+      router.get('/signup', this.signupPage);
     },
     indexPage: function(req, res) {
       return res.render('index', {test: 'All is working!'});
+    },
+    signupPage: function(req, res) {
+      return res.render('signup');
     }
   }
 }
